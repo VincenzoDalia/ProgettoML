ee297bf796dd0a8186375d95a1d410f6712b8dba
VincenzoDalia <ginodalia98@gmail.com>
2023-10-19 22:07:47 +0200

    analysis

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("vuoto")
+from Utils.load import *
+
+
+if __name__ == '__main__':
+
+    # Loading Dataset
+
+    training_data, training_label = load('./Dataset/Train.txt')
+    test_data, test_label = load('./Dataset/Test.txt')
+
+    # Number of Males and Females of training and test sets
+
+    num_male_train = numpy.count_nonzero(training_label == 0)
+    num_female_train = numpy.count_nonzero(training_label == 1)
+    num_male_test = numpy.count_nonzero(test_label == 0)
+    num_female_test = numpy.count_nonzero(test_label == 1)
+
+    print(
+        "        TRAIN     TEST\n" +
+        f"MALE     {num_male_train}      {num_male_test}\n" +
+        f"FEMALE   {num_female_train}     {num_female_test}\n ")
