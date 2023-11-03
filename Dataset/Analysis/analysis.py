986ba29245907772f9d898e2c400eab94df0083d
MarioTodaro <mario.todaro21@gmail.com>
2023-11-03 14:41:58 +0100

    heatmaps updated

--- Dataset/Analysis/analysis.py
+++ Dataset/Analysis/analysis.py
@@ -195,6 +195,35 @@ def plot_features_histograms(DTR, LTR, _title):
         plt.legend()
         plt.savefig('./images/hist_' + title + '.svg')
         plt.show()
+        
+        
+def plot_heatmap(data, label, title, color, filename):
+
+    #print(f"data prima di essere tagliato: {data.shape}")
+
+    data = data[:, label]
+
+    #print(f"data dopo essere tagliato: {data.shape}")
+
+    n_features = data.shape[0]
+
+    heatmap = numpy.zeros((n_features, n_features))
+
+    for i in range(n_features):
+        for j in range(n_features):
+            if j<=i:
+                heatmap[i][j] = abs(pearsonr(data[i, :], data[j, :])[0])
+                heatmap[j][i] = heatmap[i][j]
+    
+    plt.figure()
+    plt.xticks(numpy.arange(n_features), numpy.arange(1, n_features + 1))
+    
+    plt.title("Heatmap of Pearson Correlation" + title)
+    plt.imshow(heatmap, cmap=color)
+    plt.colorbar()
+    
+    plt.savefig(filename)
+    plt.close()
 
 
 #  -------------- ANALYSIS EXE -------------- #
@@ -203,3 +232,13 @@ def plot_features_histograms(DTR, LTR, _title):
 # plot_scatter_matrix(training_data, training_label)
 # plot_LDA_hist(training_data, training_label, 1)
 # plot_heatmaps_dataset(training_data)
+
+#Plot heatmap for all the data
+plot_heatmap(training_data, range(
+    training_data.shape[1]), "", "YlGn", "Dataset/Analysis/Heatmaps/correlation_train.png")
+
+#Plot the heatmap only for male
+plot_heatmap(training_data, training_label==0, " - Male", "PuBu", "Dataset/Analysis/Heatmaps/male_correlation_train.png")
+
+#Plot the heatmap only for female
+plot_heatmap(training_data, training_label==1, " - Female", "YlOrRd", "Dataset/Analysis/Heatmaps/female_correlation_train.png")
--- Dataset/Analysis/heatsmaps.py
+++ Dataset/Analysis/heatsmaps.py
@@ -27,35 +27,28 @@ def plot_heatmap(data, label, color, filename):
     heatmap = numpy.zeros((n_features, n_features))
 
     for i in range(n_features):
-        for j in range(i + 1):
-            coef = abs(pearsonr(data[i, :], data[j, :])[0])
-            heatmap[i][j] = coef
-            heatmap[j][i] = coef
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(heatmap, cmap=color)
-
-    ax.set_xticks(numpy.arange(n_features))
-    ax.set_yticks(numpy.arange(n_features))
-    ax.set_xticklabels(numpy.arange(1, n_features + 1))
-    ax.set_yticklabels(numpy.arange(1, n_features + 1))
-
-    ax.set_title("Heatmap of Pearson Correlation")
-    fig.colorbar(im)
-
+        for j in range(n_features):
+            if j<=i:
+                heatmap[i][j] = abs(pearsonr(data[i, :], data[j, :])[0])
+                heatmap[j][i] = heatmap[i][j]
+    
+    plt.figure()
+    plt.xticks(numpy.arange(n_features), numpy.arange(1, n_features + 1))
+    
+    plt.title("Heatmap of Pearson Correlation")
+    plt.imshow(heatmap, cmap=color)
+    plt.colorbar()
+    
     plt.savefig(filename)
-    plt.close(fig)
+    plt.close()
 
 
-def plot_heatmaps_dataset(D):
-    color = "inferno"
-    filename = "Dataset/Analysis/Heatmaps/correlation_all.png"
-    plot_heatmap(D, range(D.shape[1]), color, filename)
-
-
+#Plot heatmap for all the data
 plot_heatmap(training_data, range(
-    training_data.shape[1]), "inferno", "Dataset/Analysis/Heatmaps/correlation_train.png")
-# plot_heatmaps_dataset(training_data)
+    training_data.shape[1]), "YlGn", "Dataset/Analysis/Heatmaps/correlation_train.png")
 
-plot_heatmap(training_data, range(
-    training_data.shape[1]), "inferno", "Dataset/Analysis/Heatmaps/correlation_train.png")
+#Plot the heatmap only for male
+plot_heatmap(training_data, training_label==0, "PuBu", "Dataset/Analysis/Heatmaps/male_correlation_train.png")
+
+#Plot the heatmap only for female
+plot_heatmap(training_data, training_label==1, "YlOrRd", "Dataset/Analysis/Heatmaps/female_correlation_train.png")
