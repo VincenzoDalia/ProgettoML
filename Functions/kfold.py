6ec13679b181926b8a3a9abd0889e4ad5e6c6ba0
MarioTodaro <mario.todaro21@gmail.com>
2023-12-06 14:26:22 +0100

    kfold nostrale

--- Functions/kfold.py
+++ Functions/kfold.py
@@ -1,37 +1,33 @@
 import numpy as np
 
-
 def kfold(D, L, model, k, eff_prior=None, seed=3):
     SPost_partial = []
-    folds = []
-
+    
+    N = D.shape[1]
+    fold_size = N // k  # Calcolo quanti samples ci sono in ogni fold al massimo.
+    remainder = N % k  # Calcolo il numero di elementi di resto
+    
     np.random.seed(seed)
-    idx = np.random.permutation(D.shape[1])
+    idx = np.random.permutation(N)
 
     Label = L[idx]
 
-    fold_size = D.shape[1] // k
-
-    # Divide indices into k-folds
+    start_idx = 0
     for i in range(k):
-        start = i * fold_size
-        end = (i + 1) * fold_size
-        folds.append(idx[start:end])
+        # Calcolo la dimensione del fold corrente, considerando gli elementi di resto
+        current_fold_size = fold_size + 1 if i < remainder else fold_size
 
-    # If the number of samples is not divisible by K, add the remaining samples to the last fold
-    if D.shape[1] % k != 0:
-        folds[-1] = np.concatenate((folds[-1], idx[k * fold_size :]))
+        test_indices = idx[start_idx: start_idx + current_fold_size]
+        train_indices = np.concatenate((idx[:start_idx], idx[start_idx + current_fold_size:]))
 
-    # Perform Cross validation
-    for i in range(k):
-        # Choose the i-th fold as the validation fold
-        validation_indices = folds[i]
-        train_indices = np.concatenate([folds[j] for j in range(k) if j != i])
+        # Aggiorno l'indice di partenza per il prossimo fold
+        start_idx += current_fold_size
+
         model.train(
             D[:, train_indices],
             L[train_indices],
-            D[:, validation_indices],
-            L[validation_indices],
+            D[:, test_indices],
+            L[test_indices],
             eff_prior,
         )
         model.calculate_scores()
