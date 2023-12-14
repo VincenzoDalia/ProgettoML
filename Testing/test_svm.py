4d2677f1a8f4ba8824ccd1fba00ba6bd2f134a84
VincenzoDalia <ginodalia98@gmail.com>
2023-12-14 16:22:03 +0100

    Test SVM PLOT

--- Testing/test_svm.py
+++ Testing/test_svm.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 from Metrics.BayesErr import *
 from Calibration.Calibrate import *
 
+
+
+
 def SVM_candidate_test(DTR,LTR,DTE,LTE):
     
     DTR_znorm = znorm(DTR)
@@ -41,3 +44,47 @@ def calibrated_SVM_test_dcf(DTR, LTR, DTE, LTE, prior):
     act_dcf = DCF(prior, conf_matrix, 1, 1, True)
     
     print(f"Radial SVM (Test) {prior}     min_dcf: {round(min_dcf, 3)}        act_dcf: {round(act_dcf, 3)}")
+    
+    
+def RadialSVM_EVAL(DTR, LTR, DTE, LTE, prior, ZNorm=False):
+    
+    string_to_append = "_raw"
+    
+    if ZNorm:
+        DTR = znorm(DTR)
+        DTE = znorm(DTE)
+        string_to_append = "_znorm"
+    
+    C_values = numpy.logspace(-5, 5, num=11)
+    gamma_values = [0.1, 0.01, 0.001]
+    min_dcf_results = {gamma: {'val': [], 'eval': []} for gamma in gamma_values}
+
+    for gamma in gamma_values:
+        for c in C_values:
+            svm = Radial_SVM(1, c, gamma)
+
+            SPost, Label = kfold(svm, 5, DTR, LTR, prior)
+            res_val = MIN_DCF(0.5, 1, 1, Label, SPost)
+            min_dcf_results[gamma]['val'].append(res_val)
+
+            svm.train(DTR, LTR, DTE, LTE, 0.5)
+            svm.compute_scores()
+            scores = svm.scores
+            res_eval = MIN_DCF(0.5, 1, 1, LTE, scores)
+            min_dcf_results[gamma]['eval'].append(res_eval)
+
+    plt.figure()
+    plt.xlabel("C")
+    plt.xscale("log")
+    plt.ylabel("minDCF")
+    plt.title("RKB-SVM")
+
+    colors = ['red', 'blue', 'green']
+    for gamma, color in zip(gamma_values, colors):
+        plt.plot(C_values, min_dcf_results[gamma]['val'], label=f"logγ = {numpy.log10(gamma)} (VAL)", color=color)
+        plt.plot(C_values, min_dcf_results[gamma]['eval'], label=f"logγ = {numpy.log10(gamma)} (EVAL)", color=color, linestyle="dashed")
+
+    plt.xlim(C_values[0], C_values[-1])
+    plt.legend()
+    plt.savefig(f"Testing/RadialSVM{string_to_append}.pdf")
+    plt.close()
